fcc6d37b9a2bdf3f87ef001a45f33e831147f2c7
mayurrmehesare <138973328+mayurrmehesare@users.noreply.github.com>
2026-01-08 13:48:53 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from Apache Camel running using RPM on aws successfully 🚀";
+        return "Hello from Apache Camel running using RPM on aws successfully today 🚀";
     }
 }
